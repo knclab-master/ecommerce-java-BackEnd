ec1f81ec57f7d7cb24c7e5aa2aa2dfd4f89d356a
Cristian <cbraidait@gmail.com>
2025-07-31 22:08:41 -0300

    CORS

---
+++ src/main/java/org/KnC/Lab/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package org.KnC.Lab.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("*") // O poner sólo "http://localhost:5500" si usas Live Server
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
